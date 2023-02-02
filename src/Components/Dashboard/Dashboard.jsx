c66eba523a29cba5d7fb015074d88cb763fc71ce
AranbMozumdar <arnab.mozumdar@digitalaptech.co.in>
2023-02-02 16:17:57 +0530

    button on dashboard

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
 
 const Dashboard = () => {
   return (
@@ -6,6 +8,12 @@ const Dashboard = () => {
     <h1>Hello.....</h1>
     <br></br>
     <h2>Welcome to Dashboard</h2>
+    <br></br>
+    <Link to="/login">
+        <button>Log out</button>
+      </Link>
+  
+    
     </div>
   )
 }
